694941cb04ba1b56eeff8ca6d10b3c50da6e1fa2
noahOU <157321555+noahOU@users.noreply.github.com>
2024-10-02 00:34:58 -0500

    Create model-instructors-with-courses.php

---
+++ model-instructors-with-courses.php
@@ -0,0 +1,15 @@
+<?php
+function selectInstructors() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT instructor_id, instructor FROM hw3.instructor");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
