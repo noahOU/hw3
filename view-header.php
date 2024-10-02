8169046c7005e91662c28dfce1109ad4465eef80
noahOU <157321555+noahOU@users.noreply.github.com>
2024-10-01 23:48:24 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -20,7 +20,7 @@
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="instructors.php">Instrcutors</a>
+          <a class="nav-link" href="instructors.php">Instructors</a>
         </li>
          <li class="nav-item">
           <a class="nav-link" href="courses.php">Courses</a>
