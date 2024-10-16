9bc75299cee1657b370ef85b588ea79bc6ab0961
noahOU <157321555+noahOU@users.noreply.github.com>
2024-10-16 13:12:42 -0500

    Update model-courses.php

--- model-courses.php
+++ model-courses.php
@@ -1,8 +1,8 @@
 <?php
-function selectCourses() {
+function selectEmployees() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT course_id, course_number, course_description FROM hw3.course");
+        $stmt = $conn->prepare("SELECT employee_id, employee_name, email FROM hw3.employee");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
